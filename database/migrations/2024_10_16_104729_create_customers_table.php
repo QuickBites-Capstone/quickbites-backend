6c2d56e91e857de82f9449024100bee6b7c858d3
jpynzon <joshuapauloynzon@gmail.com>
2024-10-16 18:56:36 +0800

    created migrations

---
+++ database/migrations/2024_10_16_104729_create_customers_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomersTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('email')->unique()->nullable();
+            $table->integer('balance')->unique()->nullable();
+            $table->string('password')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
