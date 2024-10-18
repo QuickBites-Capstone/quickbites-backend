758c4ade9a9c15458758dfcd9b3b41d12a86dc60
jpynzon <joshuapauloynzon@gmail.com>
2024-10-18 11:06:38 +0800

    reason controller, customer controller

--- routes/api.php
+++ routes/api.php
@@ -5,12 +5,18 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ReasonController;
+use App\Http\Controllers\CustomerController;
 
 Route::get('/admins', [AdminController::class, 'index']);
 Route::get('/roles', [RoleController::class, 'index']);
 
-Route::get('/orders', [OrderController::class, 'index']);
+Route::get('/customers', [CustomerController::class, 'search']);
 
+Route::get('/reasons', [ReasonController::class, 'index']);
+
+Route::get('/orders', [OrderController::class, 'index']);
+Route::post('/orders', [OrderController::class, 'store']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
