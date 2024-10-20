7204dff033deef4b056cc1031ba16d67d73e48eb
jpynzon <joshuapauloynzon@gmail.com>
2024-10-21 01:00:24 +0800

    added links in emails

--- resources/views/emails/admin-credentials.blade.php
+++ resources/views/emails/admin-credentials.blade.php
@@ -8,5 +8,8 @@
     <p>Your admin account has been created successfully.</p>
     <p><strong>Email:</strong> {{ $admin['email'] }}</p>
     <p><strong>Password:</strong> {{ $password }}</p>
+    <p>
+        Go to <a href="http://localhost:3000/admincms">AdminCMS</a>
+    </p>
 </body>
 </html>
--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -8,5 +8,8 @@
     <h2>Hi, {{ $customer['first_name'] }} {{ $customer['last_name'] }}!</h2>
     <p>Get ready to explore our delicious menu and pre-order your favorite meals effortlessly!</p>
     <p>We look forward to serving you!</p>
+    <p>
+        <a href="http://localhost:3000/">QuickBites.com</a>
+    </p>
 </body>
 </html>
