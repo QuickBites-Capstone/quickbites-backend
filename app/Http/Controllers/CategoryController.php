e2274a5506328a225927830f329d00b91baff458
jpynzon <joshuapauloynzon@gmail.com>
2024-10-24 10:52:33 +0800

    use image_url

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,22 +4,33 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Category;
+use App\Http\Services\ImageService;
 
 class CategoryController extends Controller
 {
+    protected $imageService;
+
+    public function __construct(ImageService $imageService)
+    {
+        $this->imageService = $imageService;
+    }
     public function index()
     {
         $categories = Category::all();
         return response()->json($categories);
     }
-    public function getProductsByCategory($categoryId){
+    public function getProductsByCategory($categoryId)
+    {
         $category = Category::find($categoryId);
 
-        if(!$category){
+        if (!$category) {
             return response()->json(['error' => 'Category not found!'], 404);
         }
 
-        $products = $category->products;
+        $products = $category->products->map(function ($product) {
+            $product->image_url = $this->imageService->getTemporaryImageUrl($product->image);
+            return $product;
+        });
 
         return response()->json($products);
     }
