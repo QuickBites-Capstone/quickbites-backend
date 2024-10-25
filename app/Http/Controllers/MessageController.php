326fa69c9715c70b97ed959952289c29e76e35a7
jpynzon <joshuapauloynzon@gmail.com>
2024-10-25 10:10:43 +0800

    real time notification

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -10,8 +10,6 @@ class MessageController extends Controller
     public function sendMessage(Request $request)
     {
         $message = $request->input('message');
-
-        // Dispatch the job to queue the event
         SendMessageJob::dispatch($message);
 
         return response()->json(['status' => 'Message sent!']);
