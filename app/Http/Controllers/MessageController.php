08673a532f68553f48a9d61a6312bdae6261bbbf
jpynzon <joshuapauloynzon@gmail.com>
2024-10-26 23:06:18 +0800

    removed console log and comments

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -9,21 +9,18 @@ class MessageController extends Controller
 {
     public function sendMessage(Request $request)
     {
-        // Validate request inputs
         $request->validate([
             'icon' => 'required|string',
             'header' => 'required|string',
             'message' => 'required|string',
-            'customer_id' => 'required|integer|exists:customers,id', // Assuming you have a customers table
+            'customer_id' => 'required|integer|exists:customers,id',
         ]);
 
-        // Retrieve inputs
         $icon = $request->input('icon');
         $header = $request->input('header');
         $message = $request->input('message');
         $customerId = $request->input('customer_id');
 
-        // Dispatch the message job with customer ID
         SendMessageJob::dispatch($icon, $header, $message, $customerId);
 
         return response()->json(['status' => 'Message sent!']);
