996c079f9a81a55bf8d8617ccde649aec5c3bd5b
jpynzon <joshuapauloynzon@gmail.com>
2024-11-05 17:50:36 +0800

    image url for popular items

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Order;
 use App\Models\Cart;
+use App\Http\Services\ImageService;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\JsonResponse;
 
@@ -26,15 +27,33 @@ class DashboardController extends Controller
         return response()->json($data);
     }
 
+    protected $imageService;
+
+    public function __construct(ImageService $imageService)
+    {
+        $this->imageService = $imageService;
+    }
+
     public function getTopSellingItems(): JsonResponse
     {
         $topSellingItems = DB::table('cart_items')
             ->join('products', 'cart_items.product_id', '=', 'products.id')
-            ->select('products.id', 'products.name', 'products.image', 'products.price', DB::raw('SUM(cart_items.quantity) as total_sold'))
+            ->select(
+                'products.id',
+                'products.name',
+                'products.image',
+                'products.price',
+                DB::raw('SUM(cart_items.quantity) as total_sold')
+            )
             ->groupBy('products.id', 'products.name', 'products.image', 'products.price')
             ->orderByDesc('total_sold')
             ->limit(5)
             ->get();
+
+        $topSellingItems->transform(function ($item) {
+            $item->image_url = $this->imageService->getTemporaryImageUrl($item->image);
+            return $item;
+        });
 
         return response()->json($topSellingItems);
     }
